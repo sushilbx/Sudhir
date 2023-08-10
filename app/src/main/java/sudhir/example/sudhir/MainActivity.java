311f5a45ea711f7607957a8e8ea5a640111e2b1c
CHAHAT <bxrsushil@gmail.com>
2023-08-11 02:06:28 +0530

    Initial commit

---
+++ app/src/main/java/sudhir/example/sudhir/MainActivity.java
@@ -0,0 +1,14 @@
+package sudhir.example.sudhir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
